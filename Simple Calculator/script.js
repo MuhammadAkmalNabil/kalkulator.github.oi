9a132401b27bb86a08760a91ef58b8a02988ada5
Akmal Nabil <muhammadakmalnabil24@gmail.com>
2023-05-17 13:08:37 +0700

    first commit

---
+++ Simple Calculator/script.js
@@ -0,0 +1,22 @@
+
+// Insert angka ke textview
+function insert(num){
+	document.form.textarea.value = document.form.textarea.value + num;
+}
+
+// Fungsi Hitung
+function equal(){
+	var hasil = document.form.textarea.value;
+	document.form.textarea.value = eval(hasil);
+}
+
+// Clean (Menghapus semua)
+function clean(){
+	document.form.textarea.value = "";
+}
+
+// Delete (Menghapus angka terakhir)
+function back(){
+	var hasil = document.form.textarea.value;
+	document.form.textarea.value = hasil.substring(0,hasil.length-1);
+}
